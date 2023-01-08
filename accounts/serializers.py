a80e3e38f3e9b6cedfbfbb67b7f6704561039ff8
Michael Renken <michaelarenken@gmail.com>
2023-01-08 07:14:46 -0600

    Fix formatting

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 
 from .models import Account
 
+
 class AccountSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Account
         fields = [
-            'username',
-            'first_name',
-            'last_name',
-            'email',
+            "username",
+            "first_name",
+            "last_name",
+            "email",
         ]
-        read_only_fields = ['username', 'first_name', 'last_name', 'email']
-
+        read_only_fields = ["username", "first_name", "last_name", "email"]
